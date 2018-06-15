e8acdf49b7644bfc537b0366cf86cb71d28793f8
Carson Kelley <carsonmkelley@gmail.com>
2018-06-15 11:48:27 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -13,4 +13,20 @@ const codes = [
 
 function init() {
   // your code here
+  document.addEventListener('keydown',onKeyDownHandler);
+  let index = 0;
+  function onKeyDownHandler(e) {
+  	const key = e.key;
+  	console.log(key);
+  	if(key === codes[index]) {
+  		index++;
+  		if(index === codes.length) {
+  			alert("Yeah!");
+  			
+  			index = 0;
+  		}
+  	} else {
+  		index = 0;
+  	}
+  }
 }
